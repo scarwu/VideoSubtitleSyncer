861faf6acad1f38d28fe09eee4284f7de55ca9af
Scar Wu <xneriscool@gmail.com>
2020-06-01 01:09:48 +0800

    1. remove useless files & code

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import App from "./App.vue";
+import App from "./app.vue";
 
 const app = createApp(App);
 app.mount("#app");
@@ -7,8 +7,8 @@ app.mount("#app");
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://www.snowpack.dev/#hot-module-replacement
 if (import.meta.hot) {
-  import.meta.hot.accept();
-  import.meta.hot.dispose(() => {
-    app.unmount();
-  });
+    import.meta.hot.accept();
+    import.meta.hot.dispose(() => {
+        app.unmount();
+    });
 }
